52e2e98726323d1b5fd1a4aa8d098fe05c8ab00d
valeravsht <valeravsht@gmail.com>
2018-10-01 20:44:53 +0400

    20181001

--- main/main.py
+++ main/main.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 from itertools import groupby
 #from main import minusword as mw
-from myYandex.minusword import MinusWord 
+from myYandex.minusword import MinusWord,Tire 
 
 #from MinusWord import *
 
@@ -50,47 +50,181 @@ tire= [ [ "CONTINENTAL","ContiIceContact", '2' ,  "" ,  '175',    '65',    '14']
   ["CONTINENTAL",    "ContiIceContact", '2', "SUV",  '225' ,   '65',    '17'],    
   ["CONTINENTAL",    "ContiIceContact", '2', "SUV",  '235' ,   '55',    '20'],    
   ["CONTINENTAL",    "ContiIceContact", '2', "SUV" , '245' ,   '55' ,   '19'],    
-  ["CONTINENTAL",    "ContiIceContact", '2', "SUV",  '265' ,   '60' ,   '18']]   
-word1 = ['шина зимняя','купить шина зимняя']
-
-minusW = MinusWord.create(tire)
-for key in minusW:
-    print(minusW[key])
+  ["CONTINENTAL",    "ContiIceContact", '2', "SUV",  '265' ,   '60' ,   '18']] 
+  
+listWord1 = ['шины зимние -купить -цена','купить шины зимние', 'шины зимние цена','шины -зимние -купить -цена' ]
 
 
-# 
-# def createMinusWord(arrWord =[] ):
-#     minusWord = {}
-#     tmpstr = ''
-#     for t in arrWord:
-#         i=1
-#         print(t)
-#         for iw in t:
-#             #print(iw)
-#             tmpstr = ''
-#             for tmpWord in t[:i]:
-#                 tmpstr += tmpWord
-#                 if tmpWord !=  t[:i][-1] and len(tmpWord)>0:#не последнее и не пустое
-#                     tmpstr += " "
-#             # print('!'+tmpstr+'!')     
-#             #print(t[:i],t[i:])
-#             for tmpMw in t[i:]:
-#                 if len(tmpMw)>0:#не пустое
-#                     minusWord[tmpstr].apend(tmpMw)
-#             i+=1
-#     # уберем дубликаты
-#     for key in minusWord:
-#         minusWord[key]=  [el for el, _ in groupby(minusWord[key])]        
-#     return minusWord
-        #print(tire[t][i]) 
-#     
-#     print("{0} -{1} -{2} -{3} -{4} -{5} -{6}".format(tire[t][0],tire[t][1],tire[t][2],tire[t][3],tire[t][4],tire[t][5],tire[t][6]))
-#     print("{0} {1} -{2} -{3} -{4} -{5} -{6}".format(tire[t][0],tire[t][1],tire[t][2],tire[t][3],tire[t][4],tire[t][5],tire[t][6]))
-#     print("{0} {1} {2} -{3} -{4} -{5} -{6}".format(tire[t][0],tire[t][1],tire[t][2],tire[t][3],tire[t][4],tire[t][5],tire[t][6]))
-#     print("{0} {1} {2} {3} -{4} -{5} -{6}".format(tire[t][0],tire[t][1],tire[t][2],tire[t][3],tire[t][4],tire[t][5],tire[t][6]))
-#     print("{0} {1} {2} {3} {4} -{5} -{6}".format(tire[t][0],tire[t][1],tire[t][2],tire[t][3],tire[t][4],tire[t][5],tire[t][6]))
-#     print("{0} {1} {2} {3} {4} {5} -{6}".format(tire[t][0],tire[t][1],tire[t][2],tire[t][3],tire[t][4],tire[t][5],tire[t][6]))
-#     print("{0} {1} {2} {3} {4} {5} {6}".format(tire[t][0],tire[t][1],tire[t][2],tire[t][3],tire[t][4],tire[t][5],tire[t][6]))
-#                  
-    # print("{0} {1} {2} {3} {4} {5} {6}".format(tire[t])) 
+'''
+icecontact,
+contivikingcontact,vikingcontact, viking contact,
+
+contiwintercontact ,wintercontact
+'''
+tire1=[]
+
+#переведем все в нижний регистр
+for t in tire:
+    a = MinusWord.comp( ['CONTINENTAL',"", '' ,  "" ,  '',    '',    ''],t)
+    if a:
+        t_new = t.copy()
+        t_new[0]=t_new[0].lower()
+        t_new[1]=t_new[1].lower()
+        #   t_new[2]=""
+        t_new[4]=t_new[4].lower()
+        #  t_new[6] = 'r'+t_new[6]
+        print(t_new)
+        tire1.append(t_new.copy())
+        
+# уберем 2 и suv       
+for t in tire:
+    a = MinusWord.comp( ['CONTINENTAL',"", '' ,  "" ,  '',    '',    ''],t)
+    if a:
+        t_new = t.copy()
+        t_new[0]=t_new[0].lower()
+        t_new[1]=t_new[1].lower()
+        t_new[2]=""
+        t_new[4]=''
+        #  t_new[6] = 'r'+t_new[6]
+        print(t_new)
+        tire1.append(t_new.copy())
+        
+# заменим ContiIceContact  на    icecontact
+for t in tire:
+    a = MinusWord.comp( ['CONTINENTAL',"", '' ,  "" ,  '',    '',    ''],t)
+    if a:
+        t_new = t.copy()
+        t_new[0]=t_new[0].lower()
+        t_new[1]='icecontact'
+        t_new[2]=""
+        t_new[4]=''
+        #  t_new[6] = 'r'+t_new[6]
+        print(t_new)
+        tire1.append(t_new.copy())
+
+# заменим ContiIceContact  на    contivikingcontact
+for t in tire:
+    a = MinusWord.comp( ['CONTINENTAL',"", '' ,  "" ,  '',    '',    ''],t)
+    if a:
+        t_new = t.copy()
+        t_new[0]=t_new[0].lower()
+        t_new[1]='contivikingcontact'
+        t_new[2]=""
+        t_new[4]=''
+        #  t_new[6] = 'r'+t_new[6]
+        print(t_new)
+        tire1.append(t_new.copy())   
+        
+# заменим ContiIceContact  на    vikingcontact
+for t in tire:
+    a = MinusWord.comp( ['CONTINENTAL',"", '' ,  "" ,  '',    '',    ''],t)
+    if a:
+        t_new = t.copy()
+        t_new[0]=t_new[0].lower()
+        t_new[1]='vikingcontact'
+        t_new[2]=""
+        t_new[4]=''
+        #  t_new[6] = 'r'+t_new[6]
+        print(t_new)
+        tire1.append(t_new.copy())
+           
+# заменим ContiIceContact  на    viking contact
+for t in tire:
+    a = MinusWord.comp( ['CONTINENTAL',"", '' ,  "" ,  '',    '',    ''],t)
+    if a:
+        t_new = t.copy()
+        t_new[0]=t_new[0].lower()
+        t_new[1]='viking'
+        t_new[2]="contact"
+        t_new[4]=''
+        #  t_new[6] = 'r'+t_new[6]
+        print(t_new)
+        tire1.append(t_new.copy())
+           
+# заменим ContiIceContact  на    contiwintercontact
+for t in tire:
+    a = MinusWord.comp( ['CONTINENTAL',"", '' ,  "" ,  '',    '',    ''],t)
+    if a:
+        t_new = t.copy()
+        t_new[0]=t_new[0].lower()
+        t_new[1]='contiwintercontact'
+        t_new[2]=""
+        t_new[4]=''
+        #  t_new[6] = 'r'+t_new[6]
+        print(t_new)
+        tire1.append(t_new.copy())   
+        
+# заменим ContiIceContact  на    wintercontact
+for t in tire:
+    a = MinusWord.comp( ['CONTINENTAL',"", '' ,  "" ,  '',    '',    ''],t)
+    if a:
+        t_new = t.copy()
+        t_new[0]=t_new[0].lower()
+        t_new[1]='wintercontact'
+        t_new[2]=""
+        t_new[4]=''
+        #  t_new[6] = 'r'+t_new[6]
+        print(t_new)
+        tire1.append(t_new.copy()) 
+        
+tire2 = []
+          
+# добавим еще и r диметр
+for t in tire1:
+    a = MinusWord.comp( ['continental',"", '' ,  "" ,  '',    '',    ''],t)
+    if a:
+        t_new = t.copy()
+        tire2.append(t.copy())  
+        t_new[6] = 'r'+t_new[6]
+        print(t_new)
+        tire2.append(t_new.copy()) 
+        
+print(len(tire2))   
+
+minusW = MinusWord.create(tire2)
+
+print(len(minusW)) 
+
+''' Фраза (с минус-словами) -  Unnamed: 8
+    Заголовок 1                Unnamed: 11  
+    Заголовок 2                Unnamed: 12 
+    Текст                      Unnamed: 13
+    Ссылка                     Unnamed: 17
+    Отображаемая ссылка        Unnamed: 18
+'''
+    
+os.chdir('d:\\1')
+ 
+df = pd.read_excel(open('direct_example.xls','rb'),sheet_name='txt', encoding='utf-8')
+d=[]
+
+df2=df.ix[9].copy()
+
+for word1 in listWord1:
+    for word2 in minusW:
+        df2.ix[ 'Unnamed: 8'] = ' ' + word1 + ' ' +  " ".join(minusW[word2][0]) + ' -' + " -".join(minusW[word2][1]) 
+        df2.ix[ 'Unnamed: 11'] = 'Акция! Бесплатный шиномонтаж на шины Continental' 
+        df2.ix[ 'Unnamed: 12'] = 'Достоверно. Додротно. Доступно.' 
+        df2.ix[ 'Unnamed: 13'] = 'Доступные цены. Акция Бесплатный шиномонтаж на шины Contintntal'
+        df2.ix[ 'Unnamed: 17'] = 'http:\\shintorg.pro'  
+        df = df.append(df2.copy())
+
+# df2.ix[ 'Unnamed: 8'] = 'fpodspof poipiwerpo -jelej'
+# df2.ix[ 'Unnamed: 11'] = 'УРА'
+# df2.ix[ 'Unnamed: 12'] = 'Ууря' 
+# df = df.append(df2.copy())
+# df2.ix[ 'Unnamed: 8'] = 'fpodspof poipiwerpo -jelej!!!!!!!!!!!!!'
+# df2.ix[ 'Unnamed: 11'] = 'УРА!!!!!!!!!!!!!!'
+# df2.ix[ 'Unnamed: 12'] = 'Ууря!!!!!!!!!!!!!' 
+# df = df.append(df2.copy())
+#  
+# print(df.keys())
+# print(df['Unnamed: 2'])
+# print(df.ix[18,'Unnamed: 3'])
+# df.ix[18,'Unnamed: 3'] = 88888888
+# print(df.ix[18,'Unnamed: 3'])
+print(df.shape)
+df.to_excel('d:\\1\\contic.xls', index=False)
+
+print('Все')
 
--- myYandex/minusword.py
+++ myYandex/minusword.py
@@ -43,6 +43,7 @@ class MinusWord(object):
                 tmpstr = '' # для ключа словоря
                 word = [] # масив для поисковых слов
                 for tmpWord in t[:i]: # формируем поисковые слова
+                    
                     if tmpWord !=  t[:i][0] and len(tmpWord)>0:#не последнее и не пустое
                         tmpstr += " "    
                     if len(tmpWord)>0:# не пустое                    
@@ -52,7 +53,12 @@ class MinusWord(object):
                 if tmpstr not in minusWord: # новая поисковая фраза
                     minusWord[tmpstr]=[]
                     minusWord[tmpstr].append([])# 0 слова
-                    minusWord[tmpstr].append([])# 1 минус слова        
+                    minusWord[tmpstr].append([])# 1 минус слова   
+                    tire = Tire()
+                    tire.brand='continental'
+                    minusWord[tmpstr].append(tire)
+                    
+                         
                 minusWord[tmpstr][0].append(word.copy())         
                 for tmpMw in t[i:]: # формируем минусовые слова
                     if len(tmpMw)>0:#не пустое
@@ -79,11 +85,22 @@ class MinusWord(object):
             tmpList = minusWord[key][1].copy()
             tmpList.sort()
             tmpList1 = [el for el, _ in groupby(tmpList)] 
-            minusWord[key][1]=  tmpList1.copy()      
+            minusWord[key][1]=  tmpList1.copy()  
+            
+            tmpList = minusWord[key][0].copy()
+            tmpList.sort()
+            tmpList1 = [el for el, _ in groupby(tmpList)] 
+            minusWord[key][0]=  tmpList1.copy()   
+        
+        #уберем лишний уровень масивов
+        for key in minusWord:
+            tmpList = minusWord[key][0].copy()
+            minusWord[key][0]=tmpList[0].copy()
+            
         return minusWord   
     
-   
-    def comp(self,templ_list=[],sour_list=[]):
+    @staticmethod    
+    def comp(templ_list=[],sour_list=[]):
         '''
         сравнивает  список templ_list c sour_list
         если не пустые элементы масива равны возвращант  True
@@ -103,5 +120,98 @@ class MinusWord(object):
             i+=1
                 
         return ret
+
+class Tire(object):
+    def __init__(self):
+        self._width = None 
+        self.__height=None
+        self.__diameter = None
+        self.__brand = None 
+        self.__season = None
+
+    def get_brand(self):
+        return self.__brand
+
+
+    def get_season(self):
+        return self.__season
+
+
+    def set_brand(self, value):
+        self.__brand = value
+
+
+    def set_season(self, value):
+        self.__season = value
+
+
+    def del_brand(self):
+        del self.__brand
+
+
+    def del_season(self):
+        del self.__season
+
+        
+        
+
+    def get_diameter(self):
+        return self.__diameter
+
+
+    def set_diameter(self, value):
+        self.__diameter = value
+
+
+    def del_diameter(self):
+        del self.__diameter
+        
+    diameter = property(get_diameter, set_diameter, del_diameter, "diameter's docstring")
+        
+    def get_height(self):
+        return self.__height
+
+
+    def set_height(self, value):
+        self.__height = value
+   
+ 
+
+    def del_height(self):
+        del self.__height
+
+    height = property(get_height, set_height, del_height, "height's docstring")
+   
+   
+     
     
+    @property
+    def width(self):
+        return self._width
+    
+    @width.setter
+    def width(self,value):
+        self._width = value
+        
+    @width.deleter
+    def width(self):
+        del self._width
+    brand = property(get_brand, set_brand, del_brand, "brand's docstring")
+    season = property(get_season, set_season, del_season, "season's docstring")
+        
+    def get_razmer(self,list):
+        ''' разбирает список list
+        если число от 12 до 22 - диаметр
+                   от 30 до 90 - профиль
+                   больше 100 - ширина
+        '''
+        if type(list) is not list:
+            return False 
+        
+        for w in list:
+            if w.isdigit():
+                a = int(w)
+                if a in range(12,22): self.__diameter=a
+                if a in range(30,90): self.__height = a
+                if a > 100:self._width=a
             
